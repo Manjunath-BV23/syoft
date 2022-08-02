d5dfdf2b8b9ee967f0569a74b6378810ea3d13c7
Manjunath B V <manjubvmtr122333@gmail.com>
2022-08-02 18:45:38 +0530

    done

--- src/Components/Product/UpdateProduct.jsx
+++ src/Components/Product/UpdateProduct.jsx
@@ -10,6 +10,7 @@ export const UpdateProduct = () => {
         description:"",
         count: 0
     })
+    const [id, setId] = useState("")
 
     const handleChange =(e)=>{
         // e.preventDefault()
@@ -19,16 +20,18 @@ export const UpdateProduct = () => {
     }
 
     const AddData = () => {
-        axios.update("https://syoft.herokuapp.com/products", product)
+        axios.update(`https://syoft.herokuapp.com/products/${id}`, product)
     }
     const mainPage = () => {
         navigate("/products")
     }
+    console.log(id)
 
     return(
         <div>
             <h2>Welcome To Update Page</h2>
             <div id = "img">
+                <input className = "input" name="id"  type="text" onChange={(e)=> setId(e.target.value)} placeholder="Product Id"/><br /> <br />
                 <input className = "input" name="name" value={product.name} type="text" onChange={handleChange} placeholder="Product name"/><br /> <br />
                 <input className = "input" name="price" value={product.price} type="text" onChange={handleChange} placeholder="Price"/><br /><br />
                 <input className = "input" name="description" value={product.description} type="text" onChange={handleChange} placeholder="Description"/><br /><br />
